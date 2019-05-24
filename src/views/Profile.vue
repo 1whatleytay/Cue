06e2ed08498ed787042493caf541aef8ab951743
Aaditya Chaudhary <walkintrash@gmail.com>
2019-05-24 14:57:26 -0400

    profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -3,8 +3,27 @@
     <div v-if="user" v-bind:class="[ `bg-${getLevelColor()}` ]"
       class="w-5/6 mx-auto rounded text-white text-center">
       <div class="text-5xl">{{ user.name }}</div>
+        <div class=" bg-gray-200 text-center"> 
+          <h1 class="text-black text-left">User profile</h1>
+          <img class="rounded-full" src="https://i.imgur.com/tnjGTBM.jpg" :height="250" align="left"/>
+          <div class="py-16 px-16 text-black rounded-sm bg-gray-100 text-lg text-left">
+            <span class="font-bold">Name:</span> {{user.name}} 
+            <br>
+            <span class="font-bold">Cards Contributed:</span> over 9000
+            <br>
+            <span class="font-bold">Title:</span> {{user.levelName}}
+            <br>
+            <span class="font-bold">Level:</span> {{user.level}}
+            <br>
+            <span class="font-bold">XP:</span> {{user.xp}}
+            
+          </div>
+          
+        </div>
+
+      </div>
     </div>
-  </div>
+  
 </template>
 
 <script>
@@ -37,11 +56,12 @@ export default {
   mounted() {
     axios.get(connection + `users/${this.$route.params.id}`).then((request) => {
       this.user = request.data
-
+      console.log(this.user)
       onSet((state) => {
         state.color = this.getLevelColor()
         state.name = null
       })
+
     })
   },
 
