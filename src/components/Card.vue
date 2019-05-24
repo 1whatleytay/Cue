dd607ed66c569c695d0687f989cf02774a9bc914
Aaditya Chaudhary <walkintrash@gmail.com>
2019-05-24 09:07:32 -0400

    Card

---
+++ src/components/Card.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class = "bg-white w-1/2 rounded h-300 text-4xl text-center mx-auto" @click = "onPress()" >
+        <br>
+        <br>
+        <br>
+        {{displayAnswer? Answer: Question}}
+        <br>
+        <br>
+        <br>
+
+        
+    </div>
+    
+
+</template>
+
+<script>
+export default {
+    props: [ "Question","Answer" ],
+    methods: {
+        onPress() {
+            console.log("press")
+            this.displayAnswer = !this.displayAnswer
+        }
+    },
+    data() {
+        return {
+            displayAnswer: false
+        }
+    }
+
+}
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,14 +3,19 @@
     <div></div>
     <img src="">
     <div class="text-center text-white text-5xl bg-red-500 mt-12">
-      My Center {{ john }}
+      My Center
     </div>
+    <Card Question="john" Answer="Smith">
+    </Card>
   </div>
 </template>
 
 <script>
+import Card from "../components/Card.vue"
+
 export default {
   name: 'home',
+  components: {Card},
 
   data() {
     return {
